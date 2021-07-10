3bcdb0156f00574e457750e24609abc48302c65b
henriquealansantoss <henriquealansantoss@gmail.com>
2021-07-10 10:54:15 -0300

    Curso - Aulas JavaScript/TypeScript/Node

---
+++ NodeJs/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let nome = 'Henrique';
+let idade = 30;
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos`);
---
+++ NodeJs/src/index.ts
@@ -0,0 +1,4 @@
+let nome: string = 'Henrique';
+let idade: number = 30;
+
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos`);
