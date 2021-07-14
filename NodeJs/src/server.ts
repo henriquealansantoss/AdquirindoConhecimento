41bab82938030423e04133c9843c24d7e3fc07b6
henriquealansantoss <henriquealansantoss@gmail.com>
2021-07-14 16:07:47 -0300

    Curso - Aulas JavaScript/TypeScript/Node

--- NodeJs/src/server.ts
+++ NodeJs/src/server.ts
@@ -6,4 +6,22 @@ server.get('/', (req: Request, res: Response) => {
     res.send('Olá Mundo!')
 })
 
+
+// dinamica
+server.get('/noticia/:slug', (req: Request, res: Response) => {
+    let slug: string = req.params.slug;
+    res.send(`Noticia ${slug}`)
+})
+
+server.get('/voo/:origem-:destino', (req: Request, res: Response) => {
+    // let origem = req.params.origem;
+    // let destino = req.params.destino;
+
+    let { origem, destino } = req.params;
+
+    res.send(`${origem} até ${destino}`);
+})
+
+
+
 server.listen(80);
